5a95bb7ed8e3999baee17d6749a830d55427a92b
tdw <hrbust_tdw@163.com>
2017-12-12 20:16:46 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+  	<img src="./assets/logo.png">
+  	<h1><a href="#/">开发组件库</a></h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="less" scoped>
+@import '~vux/src/styles/reset.less';
+
+body {
+  background-color: #fbf9fe;
+}
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<div>
+		<p>
+			<a href="#/btn">按钮<</a>
+		</p>
+	</div>
+	<div>
+		<p>
+			<a href="#/list">列表<</a>
+		</p>
+	</div>
+	<div>
+		<p>
+			<a href="#/nav">导航<</a>
+		</p>
+	</div>
+	</div>
+
+</template>
